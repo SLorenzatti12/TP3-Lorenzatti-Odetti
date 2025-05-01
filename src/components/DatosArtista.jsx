7a0351d875113a1afb5e9828d78fff73ab508242
Juan Ignacio Odetti <jodetti@facultad.sanfrancisco.unt.edu.ar>
2025-04-30 21:50:37 -0300

    Incorporación de css Responsive en los datos del artista

--- src/components/DatosArtista.jsx
+++ src/components/DatosArtista.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import '../styles/DatosArtista.css'
 
 export function DatosArtista() {
+
+  const navigate = useNavigate(); 
   const location = useLocation();
   const { id } = location.state || {};
 
